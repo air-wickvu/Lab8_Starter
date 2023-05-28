314b1641d2854283479384eb0a8204239851a451
Eric Vu <56463764+air-wickvu@users.noreply.github.com>
2023-05-28 14:37:13 -0700

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+Name: Eric Vu 
+1. I would place the automated tests within a Github action that runs whenever code is pushed. I think this would be the best option because it would implement the concept of Continuous Integration (CI) and the benefits of CI are that it allows for the code to be tested automatically and frequently. This option outweighs the other options because manually running tests is prone to human error and running tests after all the developlment allows for issues to be found later and can cause more challenges. 
+2. No, end to end is comphrensive of the whole system/application. 
+3. No, I think the unit test to test the message function could be seperated into multiple tests to test the different parts of the function.
+4. Yes, I would use a unit test to test the character limit of the message function because it is a small part of the function and the test would be sufficient enough to ensure the function works as expected.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,37 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('isPhoneNumber returns true for valid phone numbers', () => {
+    expect(functions.isPhoneNumber('(999)-999-9999')).toBe(true);
+    expect(functions.isPhoneNumber('999-999-9999')).toBe(true);
+    expect(functions.isPhoneNumber('999999')).toBe(false);
+    expect(functions.isPhoneNumber('9999999999999')).toBe(false);
+});
+
+test('isEmail returns true for valid emails', () => {
+    expect(functions.isEmail('bob@gmail.com')).toBe(true);
+    expect(functions.isEmail('bob@yahoo.com')).toBe(true);
+    expect(functions.isEmail('bob@gmail')).toBe(false);
+    expect(functions.isEmail('bob@gmail.')).toBe(false);
+});
+
+test('isStrongPassword returns true for valid passwords', () => {
+    expect(functions.isStrongPassword('z123')).toBe(true);
+    expect(functions.isStrongPassword('z12345678901234')).toBe(true);
+    expect(functions.isStrongPassword('z123456789012345')).toBe(false);
+    expect(functions.isStrongPassword('123456789012345')).toBe(false);
+});
+
+test('isDate returns true for valid dates', () => {
+    expect(functions.isDate('01/01/0001')).toBe(true);
+    expect(functions.isDate('1/1/0001')).toBe(true);
+    expect(functions.isDate('//')).toBe(false);
+    expect(functions.isDate('1/1/1')).toBe(false);
+});
+
+test('isHexColor returns true for valid hex colors', () => {
+    expect(functions.isHexColor('#000')).toBe(true);
+    expect(functions.isHexColor('#000000')).toBe(true);
+    expect(functions.isHexColor('#00000000000000000')).toBe(false);
+    expect(functions.isHexColor('#0')).toBe(false);
+});
